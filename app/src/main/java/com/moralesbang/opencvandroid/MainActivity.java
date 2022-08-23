9fcd1881419ab3fff8b420736f6af16ebc703b92
Royk8 <roy.maestre@udea.edu.co>
2022-08-22 19:57:15 -0500

    Face detection failed

--- app/src/main/java/com/moralesbang/opencvandroid/MainActivity.java
+++ app/src/main/java/com/moralesbang/opencvandroid/MainActivity.java
@@ -1,8 +1,6 @@
 package com.moralesbang.opencvandroid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Camera;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
@@ -13,11 +11,19 @@ import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,6 +31,9 @@ public class MainActivity extends CameraActivity {
 
     private static String LOGCAT = "OpenCV_Log";
     private CameraBridgeViewBase mOpenCvCameraView;
+    File cascFile;
+
+    CascadeClassifier faceDetector;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -32,6 +41,36 @@ public class MainActivity extends CameraActivity {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
                     Log.v(LOGCAT, "OpenCV Loaded");
+                    InputStream is = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
+                    File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+
+                    cascFile = new File(cascadeDir, "haarcascade_frontalface_alt.xml");
+
+                    try{
+                        FileOutputStream fos = new FileOutputStream(cascFile);
+
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+
+                        while((bytesRead = is.read(buffer)) != -1){
+                            fos.write(buffer, 0, bytesRead);
+                        }
+
+                        is.close();
+                        fos.close();
+                    }catch (Exception e){
+                        Log.v(LOGCAT, e.getStackTrace().toString());
+                    }
+
+                    faceDetector = new CascadeClassifier(cascFile.getAbsolutePath());
+
+                    if(faceDetector.empty()){
+                        faceDetector = null;
+                    }
+                    else{
+                        cascadeDir.delete();
+                    }
+
                     mOpenCvCameraView.enableView();
                 }
                 break;
@@ -55,7 +94,7 @@ public class MainActivity extends CameraActivity {
     }
 
     @Override
-    protected List<?extends CameraBridgeViewBase> getCameraViewList() {
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
         return Collections.singletonList(mOpenCvCameraView);
     }
 
@@ -75,14 +114,24 @@ public class MainActivity extends CameraActivity {
             Mat inputRgba = inputFrame.rgba();
             Mat inputGray = inputFrame.gray();
 
-            MatOfPoint corners = new MatOfPoint();
-            Imgproc.goodFeaturesToTrack(inputGray, corners, 20, 0.01, 10, new Mat(), 3, false);
+            /*MatOfPoint corners = new MatOfPoint();
+            Imgproc.goodFeaturesToTrack(inputGray, corners, 20, 0.01,
+                    10, new Mat(), 3, false);
             Point[] cornersArr = corners.toArray();
 
-            for(int i = 0; i < corners.row(); i++) {
+            for (int i = 0; i < corners.rows(); i++) {
                 Imgproc.circle(inputRgba, cornersArr[i], 10, new Scalar(0, 255, 0), 2);
-            }
+            }*/
 
+            MatOfRect faceDetections = new MatOfRect();
+            faceDetector.detectMultiScale(inputRgba, faceDetections);
+
+            for(Rect rect: faceDetections.toArray()){
+                Imgproc.rectangle(inputRgba, new Point(rect.x, rect.y),
+                        new Point(rect.x + rect.width, rect.y +rect.height),
+                        new Scalar(255,0,0));
+            }
+            //Core.flip(inputRgba.t(), inputRgba, 0);
             return inputRgba;
         }
     };
@@ -101,7 +150,7 @@ public class MainActivity extends CameraActivity {
     @Override
     public void onPause() {
         super.onPause();
-        if(mOpenCvCameraView != null) {
+        if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
         }
     }
@@ -109,7 +158,7 @@ public class MainActivity extends CameraActivity {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(mOpenCvCameraView != null) {
+        if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
         }
     }
